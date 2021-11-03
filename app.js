de2be0a8557f0f7ebc6d1adc78dd716a1e6fdfa2
rdfsx <m315498@gmail.com>
2021-11-03 21:08:50 +0300

    added try...catch block to app.js

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ import {searchOption} from "./src/cli/options.js";
 import {readFile, readStd} from "./src/read/reader.js";
 import {writeFile, writeStd} from "./src/write/writer.js";
 import {parseConfig} from "./src/cli/parse.js";
-import {ConfigError} from "./src/errors/validation.js";
+import {ConfigError} from "./src/errors/config.js";
 
 const config = searchOption("--config", "-c");
 const input = searchOption("--input", "-i");
 const output = searchOption("--output", "-o");
-if (config && config.match(/^((?:(?:C|R)(?:0|1)|(?:A)))+([\-](?:(?:C|R)(?:0|1)|(?:A)))*$/)) {
-    const configArray = parseConfig(config);
-
-} else {
-    throw new ConfigError("Не указан конфиг.");
-}
-if (input) {
-    readFile();
-} else if (!input) {
-    readStd();
-}
-if (output) {
-    writeFile();
-} else if (!output) {
-    writeStd();
+try {
+    if (config && config.match(/^((?:(?:C|R)(?:0|1)|(?:A)))+([\-](?:(?:C|R)(?:0|1)|(?:A)))*$/)) {
+        const configArray = parseConfig(config);
+    } else {
+        throw new ConfigError("Не указан конфиг.\n");
+    }
+    if (input) {
+        readFile();
+    } else if (!input) {
+        readStd();
+    }
+    if (output) {
+        writeFile();
+    } else if (!output) {
+        writeStd();
+    }
+} catch (e) {
+    process.stderr.write(e.message);
+    process.exit(1);
 }
